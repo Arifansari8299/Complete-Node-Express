72905fe730c792e78ed34c074285003db0c0e0fc
Arif Ansari <arifkmclu@gmail.com>
2025-07-16 20:08:05 +0530

    Added Ejs folder

---
+++ 05-ejs/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs"); // set EJS as template engine
+app.set("views", "views");     // set custom views folder (optional)
+
+app.get('/', (req, res) => {
+    res.render('home', { name: "Arif" }); // renders views/home.ejs
+});
+
+app.get('/about', (req, res) => {
+    res.render('about'); // renders views/about.ejs
+});
+app.get('/profile',(req,res)=>{
+    const user = {
+        name:"Habibullah",
+        age:18,
+        hobbies:['Hocky', 'Trading', 'driving']
+}
+ res.render('profile',{user})
+})
+
+app.listen(3000, () => {
+    console.log("Server is running at http://localhost:3000");
+});
