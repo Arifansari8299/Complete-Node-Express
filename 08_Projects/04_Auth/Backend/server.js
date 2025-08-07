9d1b4a2a57fb731b5f43ec3158bed751146d1fd3
Arif Ansari <arifkmclu@gmail.com>
2025-08-07 21:02:42 +0530

    Done Backend

--- 08_Projects/04_Auth/Backend/server.js
+++ 08_Projects/04_Auth/Backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from './config/db.js'
-// import authRoutes from './routes/authRoutes.js'
+import authRoutes from './routes/authRoutes.js'
 
 dotenv.config();
 const app = express();
@@ -15,10 +15,7 @@ app.use(express.json());
 connectDB()
 
 // Routes
-// app.use('/api/auth', authRoutes)
-app.get("/", (req, res) => {
-  res.send("hello default routes");
-});
+app.use('/api/auth', authRoutes)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
