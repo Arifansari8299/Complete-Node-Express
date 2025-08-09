71b14eb096fd592f3f3913aa3efd47482bf05f53
Arif Ansari <arifkmclu@gmail.com>
2025-08-09 10:35:48 +0530

    design the Register component

--- 08_Projects/04_Auth/Frontend/src/App.jsx
+++ 08_Projects/04_Auth/Frontend/src/App.jsx
@@ -1,20 +1,19 @@
 import React, { Suspense, lazy } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Login from "./pages/Login";
-// import Dashboard from "./pages/Dashboard";
-// import Register from "./pages/Register";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 const Register = React.lazy(()=> import("./pages/Register"));
 const Login = React.lazy(()=> import("./pages/Login"));
 const Dashboard = React.lazy(()=> import("./pages/Dashboard"))
 
 const App = () => {
+const isAuth = localStorage.getItem("token");
   return (
     <BrowserRouter>
       <Suspense fallback = {<div>Loading...</div>}>
         <Routes>
           <Route path="/" element={<Register />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard" element={isAuth ? <Dashboard /> : <Navigate to="/login"/>} />
         </Routes>
       </Suspense>
     </BrowserRouter>
--- 08_Projects/04_Auth/Frontend/src/pages/Register.jsx
+++ 08_Projects/04_Auth/Frontend/src/pages/Register.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React,{ useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Register = () => {
+  const[formData,setFormData]= useState({
+    name:"",
+    email:"",
+    password:""
+  })
+ const navigate = useNavigate()
+
+ const handleSubmit = async(e) => {
+   e.preventDefault();
+   
+  }
   return (
-    <div>Resister by lazy loading </div>
+    <form onSubmit={handleSubmit} className='max-w-md mx-auto mt-10 p-6 bg-white shadow-md '>
+     <h2 className='text-2xl font-bold text-center mb-4'>Resister</h2>
+     <input type="text" placeholder='Name' onChange={(e)=> setFormData({ ...formData, name: e.target.value })} className='w-full mb-3 p-2 border rounded-2xl' required/>
+     <input type="email" placeholder='Email' onChange={(e)=> setFormData({...formData, email: e.target.value})} className='w-full mb-3 p-2 border rounded-2xl' required />
+     <input type="password" placeholder='password' onChange={(e)=> setFormData({...formData, password: e.target.value})} className='w-full mb-3 p-2 border rounded-2xl' />
+     <button type="submit" className='w-full bg-blue-500 p-2 text-white rounded'>Submit</button>
+    </form>
   )
 }
 
