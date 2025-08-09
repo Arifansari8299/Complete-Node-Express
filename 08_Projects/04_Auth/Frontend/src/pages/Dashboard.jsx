e12b4e08341af72d048bafb395679b2dcb638856
Arif Ansari <arifkmclu@gmail.com>
2025-08-09 19:44:11 +0530

    Added Dashboard

--- 08_Projects/04_Auth/Frontend/src/pages/Dashboard.jsx
+++ 08_Projects/04_Auth/Frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Dashboard = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    navigate('/login');
+  }
   return (
-    <div>Dashboard</div>
+    <div className='p-10 text-center'>
+      <h1 className='text-3xl font-bold mb-4'>Welcome to Dashboard</h1>
+      <button onClick={handleLogout} className='bg-green-400 hover:bg-green-600 transition duration-300 cursor-pointer px-4 py-2 text-white rounded'>Logout</button>
+    </div>
   )
 }
 
