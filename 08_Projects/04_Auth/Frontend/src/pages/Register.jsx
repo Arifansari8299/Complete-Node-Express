7a10ae0c3aa2ceb3012cd7191c1bf764e36e0a87
Arif Ansari <arifkmclu@gmail.com>
2025-08-09 10:49:51 +0530

    connected to the backend database with Resister component using axios api integration

--- 08_Projects/04_Auth/Frontend/src/pages/Register.jsx
+++ 08_Projects/04_Auth/Frontend/src/pages/Register.jsx
@@ -1,27 +1,24 @@
-import React,{ useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useState } from "react"
+import axios from "axios"
+import { useNavigate } from "react-router-dom"
 
-const Register = () => {
-  const[formData,setFormData]= useState({
-    name:"",
-    email:"",
-    password:""
-  })
- const navigate = useNavigate()
+export default function Register() {
+  const [formData, setFormData] = useState({ name: "", email: "", password: "" })
+  const navigate = useNavigate()
 
- const handleSubmit = async(e) => {
-   e.preventDefault();
-   
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    await axios.post("http://localhost:5000/api/auth/register", formData)
+    navigate("/login")
   }
+
   return (
-    <form onSubmit={handleSubmit} className='max-w-md mx-auto mt-10 p-6 bg-white shadow-md '>
-     <h2 className='text-2xl font-bold text-center mb-4'>Resister</h2>
-     <input type="text" placeholder='Name' onChange={(e)=> setFormData({ ...formData, name: e.target.value })} className='w-full mb-3 p-2 border rounded-2xl' required/>
-     <input type="email" placeholder='Email' onChange={(e)=> setFormData({...formData, email: e.target.value})} className='w-full mb-3 p-2 border rounded-2xl' required />
-     <input type="password" placeholder='password' onChange={(e)=> setFormData({...formData, password: e.target.value})} className='w-full mb-3 p-2 border rounded-2xl' />
-     <button type="submit" className='w-full bg-blue-500 p-2 text-white rounded'>Submit</button>
+    <form onSubmit={handleSubmit} className="max-w-md mx-auto mt-10 p-6 bg-white shadow-md rounded">
+      <h2 className="text-2xl font-bold mb-4 text-center">Register</h2>
+      <input type="text" placeholder="Name" onChange={e => setFormData({ ...formData, name: e.target.value })} className="w-full mb-3 p-2 border rounded" required />
+      <input type="email" placeholder="Email" onChange={e => setFormData({ ...formData, email: e.target.value })} className="w-full mb-3 p-2 border rounded" required />
+      <input type="password" placeholder="Password" onChange={e => setFormData({ ...formData, password: e.target.value })} className="w-full mb-3 p-2 border rounded" required />
+      <button type="submit" className="w-full bg-blue-500 text-white p-2 rounded">Register</button>
     </form>
   )
 }
-
-export default Register
