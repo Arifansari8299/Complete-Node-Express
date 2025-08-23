685b18f2e1943344d1dcdfa6901516a19261a18e
Arif Ansari <arifkmclu@gmail.com>
2025-08-23 10:51:30 +0530

    created ui for testing speed

--- 08_Projects/06_Speedtest/client/src/App.jsx
+++ 08_Projects/06_Speedtest/client/src/App.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 function App() {
+  const[download,setDownload] = useState(null)
+  const[upload,setUpload] = useState(null)
+  const[ping,setPing] = useState(null)
+
+  const runTest = async () => {
+    setDownload('Testing');
+    setUpload('Testing');
+    setPing('Testing');
+  }
   return (
-    <div>
-      <h1 className='bg-gray-400 px-4 py-2 text-3xl text-center font-bold underline '>App</h1>
+    <div className='min-h-screen bg-gray-100 flex flex-col items-center justify-center text-gray-800'>
+      <h1 className='text-3xl font-bold mb-6 '>Internet speed test</h1>
+      <button  onClick={runTest} className='bg-blue-600 text-white px-6 py-3 rounded-md cursor-pointer font-semibold hover:bg-blue-700 transition '>start test</button>
+      <div className='text-xl space-y-4 mt-8'>
+        <p>download : {download}</p>
+        <p>upload : {upload} </p>
+        <p>ping : {ping}</p>
+      </div>
     </div>
   )
 }
