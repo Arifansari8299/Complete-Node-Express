ac36c1652cb8a6958ee67d8fd79500ad6cfb1ed8
Arif Ansari <arifkmclu@gmail.com>
2025-08-23 10:07:27 +0530

    created server

--- 08_Projects/06_Speedtest/server/server.js
+++ 08_Projects/06_Speedtest/server/server.js
@@ -1,20 +1,20 @@
-const express = require('express')
-const cors = require('cors');
-const multer = require('multer');
-const path = require('path');
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/download',express.static(path.join(__dirname,'public')));
+app.use("/download", express.static(path.join(__dirname, "public")));
 
-const upload  = multer({ limits: { fileSize: 10 * 1024 * 1024 } })
+const upload = multer({ limits: { fileSize: 10 * 1024 * 1024 } });
 
-app.post('/upload', upload.single('file'), (req, res) => {
-    res.sendStatus(200); // Just to test upload speed
-  });
+app.post("/upload", upload.single("file"), (req, res) => {
+  res.sendStatus(200); // Just to test upload speed
+});
 
-app.get('/ping', (req, res) => {
-    res.send({ timestamp: Date.now() });
-  });
-app.listen(5000, () => console.log('Server running on port 5000'));
+app.get("/ping", (req, res) => {
+  res.send({ timestamp: Date.now() });
+});
+app.listen(5000, () => console.log("Server running on port 5000"));
