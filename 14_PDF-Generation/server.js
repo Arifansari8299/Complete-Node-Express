b6385e623b52ff5ef746534ef012b7dd9807d691
Arif Ansari <arifkmclu@gmail.com>
2025-10-01 17:04:45 +0530

    generating invoice pdf from ejs

--- 14_PDF-Generation/server.js
+++ 14_PDF-Generation/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const puppeteer = require("puppeteer");
+const ejs = require('ejs')
+const path = require('path')
 const app = express();
+
+app.set('view engine','ejs')
 
 app.get("/", async (req, res) => {
   try {
@@ -45,29 +49,18 @@ app.get("/invoice", async (req, res) => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    const htmlContent = `
-          <!DOCTYPE html>
-          <head>
-      <title>PDF Generation</title>
-      <style>
-          body{
-              font-family: arial;
-          }
-          h1{
-              color: #0066cc;
-          }
-      </style>
-        </head>
-      <body>
-      <h1>PDF FILE GENERATION</h1>
-      <P>This is a pdf generated form HTML in Express.js</P>
-     </body>
-     </html>
-          `;
-    await page.setContent(htmlContent);
+    const invoiceData = {
+        invoiceNumber:1234,
+        customer:'Yahoo Baba',
+        product:'ExpressJS course',
+        price:'99'
+    }
+    const htmlContent = await ejs.renderFile(path.join(__dirname, 'views', 'invoice.ejs'),invoiceData)
+
+    await page.setContent(htmlContent,{waitUntil:'domcontentloaded'});
     const pdfBuffer = await page.pdf({
       format: "A4",
-      margin: { top: "20px", right: "20px", bottom: "20px", left: "20px" },
+      printBackground:true
     });
     await browser.close();
 
